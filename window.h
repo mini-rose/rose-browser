c8c04880fcf88867ce6f2e41c15850f8b69773f8
bellrise <bellrise.dev@gmail.com>
2022-05-21 22:22:10 +0200

    add tabs

--- window.h
+++ window.h
@@ -6,20 +6,24 @@
 #include "rose.h"
 
 typedef struct {
-    double zoom;
-    WebKitWebView *webview;
-    WebKitWebInspector *inspector;
-    WebKitFindOptions *findopts;
+	double zoom;
+	WebKitWebView *webview;
+	WebKitWebInspector *inspector;
+	WebKitFindOptions *findopts;
+	GtkEventController *controller;
 } RoseWebview;
 
 typedef struct {
-    unsigned xid;
-    short tab;
-    GtkWidget *window;
-    RoseWebview **webviews;
+	unsigned xid;
+	short tab;
+	GtkWidget *window;
+	GtkWidget *pages;
+	RoseWebview **webviews;
 } RoseWindow;
 
 RoseWindow *rose_window_new(GtkApplication *application);
 
 void rose_window_show(RoseWindow *window, const char *url);
 void rose_window_close(RoseWindow *window);
+
+RoseWebview *rose_webview_new();
