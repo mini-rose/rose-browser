0787f62e1676ebc6d295d5eb82eab8532dcf204f
fenze <fiedorowicz.kacper@gmail.com>
2022-11-23 16:58:20 +0000

    fix style formatting

--- rose.c
+++ rose.c
@@ -14,13 +14,17 @@
 
 #include <webkit2/webkit2.h>
 
-#define CACHE                                                                                      \
-	"base-cache-directory", CACHE_DIR, "base-data-directory", CACHE_DIR,                       \
-	    "disk-cache-directory", CACHE_DIR, "dom-cache-directory", CACHE_DIR,                   \
-	    "hsts-cache-directory", CACHE_DIR, "indexeddb-directory", CACHE_DIR, "itp-directory",  \
-	    CACHE_DIR, "local-storage-directory", CACHE_DIR,                                       \
-	    "offline-application-cache-directory", CACHE_DIR,                                      \
-	    "service-worker-registrations-directory", CACHE_DIR
+#define CACHE                               			\
+	"base-cache-directory", CACHE_DIR,                   	\
+	"base-data-directory", CACHE_DIR,                       \
+	"disk-cache-directory", CACHE_DIR,  			\
+	"dom-cache-directory", CACHE_DIR,       		\
+	"hsts-cache-directory", CACHE_DIR,  			\
+	"indexeddb-directory", CACHE_DIR, 			\
+	"itp-directory", CACHE_DIR, 				\
+	"local-storage-directory", CACHE_DIR,                   \
+	"offline-application-cache-directory", CACHE_DIR, 	\
+	"service-worker-registrations-directory", CACHE_DIR
 
 enum { _SEARCH, _FIND };
 
