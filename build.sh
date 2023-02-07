122437115dd0099246ce26866d808c75b36345b3
davidovki <david@davidovski.xyz>
2023-02-07 11:35:22 +0000

    Remove bash-only features for sh scripts
    Fix order of CC arguments to aid in linking on newer versions

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 CC=clang
 SRC=rose.c
 REQS= #./plugins/*/*.c
-DEPS=('webkit2gtk-4.0')
+DEPS='webkit2gtk-4.0'
 
-INCS=`pkg-config --cflags ${DEPS[@]}`
-LIBS=`pkg-config --libs ${DEPS[@]}`
+INCS=`pkg-config --cflags ${DEPS}`
+LIBS=`pkg-config --libs ${DEPS}`
 
 # Optional adblocking depends on https://github.com/jun7/wyebadblock
 WYEBAB='-L/usr/lib/wyebrowser/adblock.so'
 
 cp -f config.def.h config.h
-$CC $INCS $LIBS $REQS $SRC $WYEBAB -o rose
+$CC $INCS $REQS $SRC -o rose $LIBS $WYEBAB
