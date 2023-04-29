eb8beed6568b2d3ff1acb38606cec7d110737b67
fenze <contact@fenze.dev>
2023-04-29 12:03:26 +0200

    lua: add table fields api function

--- src/lua.c
+++ src/lua.c
@@ -1,9 +1,13 @@
 #include "lua.h"
+#include "debug.h"
 #include <lua.h>
+#include <stdlib.h>
+#include <string.h>
 
 static void rose_lua_setup()
 {
 	rose_lua_add_table("rose");
+	rose_lua_table_add_field("rose", "webkit.settings");
 }
 
 void rose_lua_add_table(char *name)
@@ -19,6 +23,35 @@ void rose_lua_add_table(char *name)
 	lua_pop(L, -1);
 	lua_newtable(L);
 	lua_setglobal(L, name);
+}
+
+void rose_lua_table_add_field(char *glob_var, const char* fieldpath)
+{
+	lua_State *L = rose_lua_state_get();
+	lua_getglobal(L, glob_var);
+
+    // Split the fieldpath string into parts
+    char* path_copy = strdup(fieldpath);
+    char* path_part = strtok(path_copy, ".");
+
+    // Traverse the fieldpath and create nested tables as needed
+    while (path_part != NULL) {
+        // Push the next table onto the stack or create it if it doesn't exist
+        lua_pushstring(L, path_part);
+        lua_gettable(L, -2);
+        if (lua_isnil(L, -1)) {
+            lua_pop(L, 1);
+            lua_newtable(L);
+            lua_pushstring(L, path_part);
+            lua_pushvalue(L, -2);
+            lua_settable(L, -4);
+        }
+        // Move to the next part of the fieldpath
+        path_part = strtok(NULL, ".");
+    }
+
+    // Clean up
+    free(path_copy);
 }
 
 lua_State *rose_lua_state_get()
--- src/lua.h
+++ src/lua.h
@@ -5,4 +5,5 @@
 #include <lauxlib.h>
 
 lua_State *rose_lua_state_get();
-void rose_lua_add_table(char *);
+void rose_lua_add_table(char *name);
+void rose_lua_table_add_field(char *glob_var, const char* fieldpath);
